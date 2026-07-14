eb8de90cc554369bbe5f84eb3e5e6589619e7247
Poojaaksha <poojahanchinmane98@gmail.com>
2026-07-14 07:49:45 +0000

    Initial Employee Management application

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("==================================");
+        Console.WriteLine(" Employee Management System");
+        Console.WriteLine("==================================");
+
+        Console.WriteLine("Employee ID   : 101");
+        Console.WriteLine("Employee Name : chaya");
+        Console.WriteLine("Department    : DevOps");
+        Console.WriteLine("Salary        : 50000");
+    }
+}
